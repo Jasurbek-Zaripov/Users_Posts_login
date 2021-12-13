dcf32aef68310c65793fd2d0a1f173cd32893bb5
Jasurbek-Zaripov <zaripovjasurbek2001@gmail.com>
2021-12-13 13:02:14 +0500

    added html files new rout <go to Home>

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const http = require('http')
-const { resolve, join } = require('path')
 const appController = require('./src/app.controller')
-const { localhost } = require(join(resolve(), 'src', 'host.js'))
+
+const localhost = '192.168.1.42'
 const server = http.createServer((require, response) => {
   // MAIN Rout
   appController(require, response)
 })
 
 server.listen(4000, () => console.log(`http://${localhost}:4000`))
-/*
-
-/
-/users
-/posts
-/posts/2
-/login
-/icon post qoldirish uchun
-windows.location
-
-*/
--- src/host.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  localhost: '192.168.1.42',
-}
